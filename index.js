550110a3267a5af995d6ffca34824e98a424a177
IngenAningVadJagSkaSkriva <123164118+IngenAningVadJagSkaSkriva@users.noreply.github.com>
2024-04-10 09:39:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var troll = () => {
-    window.open('hidden.html','','HEIGHT=1,WIDTH=1');
+    window.open('hidden.html','','HEIGHT=10,WIDTH=10');
 }
 var troll2 = () => {
-    let w = window.open('','','HEIGHT=1,WIDTH=1');
+    let w = window.open('','','HEIGHT=10,WIDTH=10');
     w.document.write('<script src="window.js"></script><img src="gay.png" style="HEIGHT: 100%; WIDTH: 100%;">');
 }
