0ae9831727639316f308267c0039be83fd3380b5
IngenAningVadJagSkaSkriva <123164118+IngenAningVadJagSkaSkriva@users.noreply.github.com>
2024-04-10 09:29:05 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var troll = () => {
+    //window.open('hidden.html','','HEIGHT=1,WIDTH=1');
+    troll2();
+}
+var troll2 = () => {
+    let w = window.open('','','HEIGHT=1,WIDTH=1');
+    w.document.write('<script src="window.js"></script><img src="gay.png" style="HEIGHT: 100%; WIDTH: 100%;">');
+}
---
+++ window.js
@@ -0,0 +1,35 @@
+function RB(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+var x = RB(0,(screen.width - window.outerWidth));
+var y = RB(0,(screen.height - window.outerHeight));
+var dupe = () => {
+    let w = window.open('','','HEIGHT=1,WIDTH=1');
+    w.document.write('<script src="window.js"></script><img src="gay.png" style="HEIGHT: 100%; WIDTH: 100%;">');
+}
+var reset = () => {
+    window.moveTo((screen.width / 2),(screen.height / 2));
+}
+window.moveTo(x,y);
+setInterval(() => {
+    dupe();
+}, 30000);
+setInterval(() => {
+    if(screenX > screen.width) {
+        reset();
+    } else if(screenX < 0) {
+        reset();
+    }
+    if(screenY > screen.height) {
+        reset();
+    } else if(screenY < 0) {
+        reset();
+    }
+    window.moveBy(RB(-10,10),RB(-10,10));
+},1);
+setTimeout(() => {
+    onfocus = () => {
+        dupe();
+        open('secondpart.html');
+    }
+}, 2500);
